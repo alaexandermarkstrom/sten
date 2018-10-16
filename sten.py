d3424c16f43836972a97e5512fe99780db89e9d9
Alexander Markström <maal16@nti.umea>
2018-10-16 11:22:44 +0200

    blyat

--- sten.py
+++ sten.py
@@ -1,10 +1,28 @@
+import random
+options = 0 #["sten", "sax", "påse"]
 sten = 1
 sax = 2
 påse = 3
+spelare = 4
+motståndare = 5
+ai = random.randint(1, 3)
+
 
 print("Hej på dig där! Välkommen till vårat program.")
 
-input(print("Här väljer du mellan sten sax och påse."))
+while options != 4:
 
-if sten
-    
+    try:
+        options = int(input("Här väljer du mellan sten sax och påse."))
+    except:
+        print ("måste välja sten, sax eller påse")
+    if options == 1:
+        print ("du har valt sten")
+    elif options == 2:
+        print ("du har valt sax")
+    elif options == 3:
+        print ("du har valt påse")
+    else:
+        print("nu avslutas programmet")
+
+
